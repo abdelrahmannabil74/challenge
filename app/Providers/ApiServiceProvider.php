eaae75cd068994691292ee6a71bdcc112db8ff87
abdelrahman <abdelrhman.nabil.ali@gmail.com>
2019-11-23 15:00:49 +0200

    creaet Api Service Provider

---
+++ app/Providers/ApiServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Providers;
+
+use App\Exceptions\ApiHandler;
+use App\Http\Responses\ApiResponder;
+use App\Http\Responses\ResponsesInterface;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Support\ServiceProvider;
+
+class ApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Number of items per page in each collection.
+     *
+     * @var int
+     */
+    public static $itemsPerPage = 10;
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Use the ApiResponder as the concrete implementation for the ResponsesInterface
+        $this->app->bind(ResponsesInterface::class, ApiResponder::class);
+        // Use the ApiHandler as the main exception handler
+        $this->app->singleton(ExceptionHandler::class, ApiHandler::class);
+    }
+}
